66f3962ca4adee40ecb53b53b0f722e0deb50cbd
craigy <craigy@users.noreply.github.com>
2016-06-12 18:34:38 -0400

    Start adding conversion back to text format.

--- src/cljs/subtitle_transform/core.cljs
+++ src/cljs/subtitle_transform/core.cljs
@@ -8,21 +8,21 @@
 
 (def ini
   (insta/parser
-    "S = tagged*
+    "<S> = tagged*
      tagged = tag line*
      tag = '['content']'
      line = '\n' content
-     content = #'[^\\[\\]]*'"))
+     <content> = #'[^\\[\\]]*'"))
 
 (defonce app-state
   (r/atom
     {:input ""
-     :output ""}))
+     :output nil}))
 
 (defn in->out [input]
   (let [parsed (ini input)]
     (if (and (not (insta/failure? parsed)) (seq parsed))
-      (str parsed)
+      parsed
       (print-str (insta/get-failure parsed)))))
 
 (defn on-change-input [event]
@@ -30,22 +30,37 @@
     (swap! app-state assoc :input input)
     (swap! app-state assoc :output (in->out input))))
 
+(defn out-trans [out]
+  (map
+    (fn [[tagged & contents]]
+      (map
+        (fn [[tag-or-line & body]]
+          (case tag-or-line
+            :tag (str "[" (apply str body) "]")
+            :line (apply str body)))
+        contents))
+    out))
+
+(def ta-style
+  {:width 400
+   :height 200
+   :resize "none"
+   :font-family "monospace"})
+
 (defn app []
   [:div {:style {:margin-left 16}}
    [:textarea
-    {:style {:width 400
-             :height 200
-             :resize "none"
-             :font-family "monospace"}
+    {:style ta-style
      :value (:input @app-state)
      :on-change on-change-input}]
    [:textarea
-    {:style {:width 400
-             :height 200
-             :resize "none"
-             :font-family "monospace"}
+    {:style ta-style
      :disabled true
-     :value (:output @app-state)}]])
+     :value (str (:output @app-state))}]
+   [:textarea
+    {:style ta-style
+     :disabled true
+     :value (out-trans (:output @app-state))}]])
 
 (defn ^:export run[]
   (r/render
