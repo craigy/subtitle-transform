8fda7085c0498098dec7259043f71c76bfb3c32c
craigy <craigy@users.noreply.github.com>
2016-06-16 21:00:01 -0400

    Improve grammar.

--- src/cljs/subtitle_transform/core.cljs
+++ src/cljs/subtitle_transform/core.cljs
@@ -9,10 +9,12 @@
 (def ini
   (insta/parser
     "<S> = tagged*
-     tagged = tag line*
-     tag = '['content']'
-     line = '\n' content
-     <content> = #'[^\\[\\]]*'"))
+     tagged = tag '\n' line*
+     tag = '[' nonclose+ ']'
+     line = nonclose #'[^\n]*' nonbracket*
+     <nonclose> = #'[^\n\\[\\]]'
+     <nonbracket> = #'[^\\[\\]]'
+     <whitespace> = #'\\s*'"))
 
 (defonce app-state
   (r/atom
